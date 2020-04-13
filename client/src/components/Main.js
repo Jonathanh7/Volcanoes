bede752235953756e6ae5d10781d10029ba6dde4
Jonathanh7 <Jonathanh949@gmail.com>
2020-04-12 20:55:40 -0400

    rendering data on main page

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -1,18 +1,37 @@
 import React, { Component } from 'react'
-import readAllVolcano from '../services/Constants'
+import { readAllVolcano } from '../services/Constants'
 
 export default class Main extends Component {
-  constructor() {
-    super();
-    this.state = {}
+  constructor(props) {
+    super(props);
+    this.state = {
+      result: [],
+      volcanic: ""
+    }
+  }
+  handleSubmit = async () => {
+    const inspired = await readAllVolcano();
+    this.setState({
+      result: inspired
+    });
+  }
+  
+  componentDidMount = async () => {
+    this.handleSubmit();
   }
 
-
   render() {
+    const { result } = this.state
+    const lava = this.state.result ? result.map((volcanoes) => {
+      return(
+        <div>{volcanoes.name}</div>
+      )
+    }) : <div></div>
+      
     return (
       <div className="landing">
         <h1 className="main-title">Volcanoes To Visit</h1>
-
+        {lava}
       </div>
     )
   }
