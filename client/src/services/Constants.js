4750c0b4e9e39458d712269c390777ac7895f467
Jonathanh7 <Jonathanh949@gmail.com>
2020-04-13 12:57:19 -0400

    annouying deployment

--- client/src/services/Constants.js
+++ client/src/services/Constants.js
@@ -1,6 +1,6 @@
 const axios = require('axios'); 
 const api = axios.create({
-  baseURL: 'http://localhost:3000'
+  baseURL: process.env.NODE_ENV === 'production' ? 'https://volcano-stuff.herokuapp.com/' : 'http://localhost:3000'
 })
 
 export const loginUser = async (loginData) => {
